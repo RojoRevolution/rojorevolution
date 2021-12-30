0d220d8fe7c51dc9d4602a0b58a33b1866a492c3
David Rojo <rojo@rojorevolution.com>
2021-12-30 17:32:45 -0600

    added placeholders for toggle section height, and got JS to work

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggleContainer = document.getElementById('toggleContainer')
     const designDIV = document.getElementById('designProjects');
     const devDiv = document.getElementById('devProjects');
+    const extras = document.getElementById('extras');
+    const extraCards = document.querySelectorAll('[card-data="extra"]')
 
     //Function for finding the Y Position of an element
     function findPos(obj) {
@@ -31,14 +33,18 @@ document.addEventListener("DOMContentLoaded", () => {
         if (designDIV.classList.contains('active')) {
             return
         }
-        toggleContainer.style.height = '1150px'
         designDIV.style.left = '0px';
         devDiv.style.right = '-1500px';
+
+        for (i = 0; i < extraCards.length; i++) {
+            extraCards[i].classList.toggle('d-none');
+        }
 
         designWorkBtn.classList.toggle('activeDesign');
         devWorkBtn.classList.toggle('activeDev');
         designDIV.classList.toggle('active')
         devDiv.classList.toggle('active')
+
     });
 
     devWorkBtn.addEventListener('click', () => {
@@ -46,14 +52,19 @@ document.addEventListener("DOMContentLoaded", () => {
         if (devDiv.classList.contains('active')) {
             return
         }
-        toggleContainer.style.height = '1700px'
+
         designDIV.style.left = '-1500px';
         devDiv.style.right = '0px';
+
+        for (i = 0; i < extraCards.length; i++) {
+            extraCards[i].classList.toggle('d-none');
+        }
 
         designWorkBtn.classList.toggle('activeDesign')
         devWorkBtn.classList.toggle('activeDev')
         designDIV.classList.toggle('active')
         devDiv.classList.toggle('active')
+
     });
 
 });
