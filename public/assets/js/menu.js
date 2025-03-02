0eed45ab42b21a16c193a458eae54d8f95330b02
David Rojo <rojo@rojorevolution.com>
2025-03-01 20:56:42 -0600

    got main nav functionalith working with animations

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const menuEl = document.getElementById('menu');
+    const openMenuEl = document.querySelector('.logo');
+    const closeMenuEl = document.querySelector('.close-menu');
+    const navItems = document.getElementById("nav-items");
+
     let currentMenuStatus
     currentMenuStatus = menuEl.getAttribute("data-menu-status");
     console.log(currentMenuStatus)
 
-    menuEl.addEventListener("click", () => {
+    openMenuEl.addEventListener("click", () => {
         console.log("Click")
         currentMenuStatus = menuEl.getAttribute('data-menu-status');
-        switch (currentMenuStatus) {
-            case "closed":
-                menuEl.setAttribute('data-menu-status', 'open');
-                break;
-            case "open":
-                menuEl.setAttribute('data-menu-status', 'closed');
-                break;
-            default:
-                break;
-        }
-
+        menuEl.setAttribute('data-menu-status', 'open');
+        navItems.classList.toggle('show');
     })
+
+    closeMenuEl.addEventListener("click", () => {
+        console.log("click");
+        menuEl.setAttribute('data-menu-status', 'closed');
+    })
+
+
 
 
 });
