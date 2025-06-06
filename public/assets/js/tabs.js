db8eabda8031ead99e062125640816d84dfa7b5e
David Rojo <rojo@rojorevolution.com>
2025-06-06 15:11:39 -0500

    css changes, starte on webdev section

--- public/assets/js/tabs.js
+++ public/assets/js/tabs.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const servicesText = {
         illustration: "My bread and butter. Illustration work is my favorite type of work to do. I work exclusively in vector graphics, ensuring the final artwork is 100% scalable. I don't subscribe to a specific style; my illustrations can vary from simple to complex, depending on the project.",
-        differentiation: "Also known as branding. Your branding should differentiate you from competitors and make you stand out in your market. Your brand will evolve over time, but its consistency should remain.",
+        differentiation: "Your branding should differentiate you from competitors and make you stand out in your market. While your brand will evolve over time, its consistency should remain. I help growing businesses establish and keep their brand standards in check.",
         logos: "You have a story to tell. Logos help tell it. A logo isn't just for your main brand. Logos can also be used for your products. Giving each product under your main brand identity can help bolster impressions and recognizability and create loyalty.",
         web: "I like to know how things work, so I learned to code. While I'm technically a full-stack developer, I mostly stick to the front-end side of development. On the freelance end, I primarily help companies get their presence online.",
         print: "I started my career in print design, and contrary to popular belief, it's not dead. So whether you need color-separated artwork for screen printing, or are looking to create unique packaging for your products, I can definitely help with that."
