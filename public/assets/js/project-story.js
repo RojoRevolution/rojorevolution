c43535f2bf7860af4072324c1be6e5da41b1045d
David Rojo <rojo@rojorevolution.com>
2026-08-21 22:44:43 -0500

    acopon content switch finished

--- public/assets/js/project-story.js
+++ public/assets/js/project-story.js
@@ -3,14 +3,40 @@ document.addEventListener("DOMContentLoaded", () => {
     const projectStorySection = document.getElementById("projectStory");
     const storyButtonEl = document.querySelectorAll("#storyBtn button");
 
+    const removeDisabledFromAllBtns = () => {
+        storyButtonEl.forEach((button) => {
+            button.setAttribute("data-active", "false");
+            button.disabled = false;
+        })
+
+    }
+
+
     storyButtonEl.forEach((button) => {
         button.addEventListener("click", (event) => {
-            // event.preventDefault();
-            let buttonClicked = event.target.getAttribute("data-active");
+            event.preventDefault();
+            let clickedButton = event.currentTarget;
+            console.log(clickedButton)
+            let content = clickedButton.getAttribute("data-content");
+            console.log(content)
+            // Set Data Attributes
+            removeDisabledFromAllBtns();
+            clickedButton.setAttribute("data-active", "true");
+            // Show Content
+            switch (content) {
+                case "work":
+                    projectWorkSection.setAttribute("data-status", "show");
+                    projectStorySection.setAttribute("data-status", "hide");
+                    break;
+                case "story":
+                    projectWorkSection.setAttribute("data-status", "hide");
+                    projectStorySection.setAttribute("data-status", "open");
+                    break;
+                default:
+                    break;
+            }
 
-            console.log(buttonClicked);
-            console.log("Click");
-
+            clickedButton.disabled = true;
         });
     })
 
