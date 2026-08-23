a0a8c91f6807f03c43bea4f14c65b75bb4880334
David Rojo <rojo@rojorevolution.com>
2026-08-23 13:26:40 -0500

    git status

--- public/assets/js/project-story.js
+++ public/assets/js/project-story.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const projectWorkSection = document.getElementById("work");
     const projectStorySection = document.getElementById("projectStory");
     const storyButtonEl = document.querySelectorAll("#storyBtn button");
+    const scrollSection = document.querySelector(".scroll-section ")
+    const bodyEl = document.querySelector("body");
+
+    console.log(scrollSection);
 
     const removeDisabledFromAllBtns = () => {
         storyButtonEl.forEach((button) => {
             button.setAttribute("data-active", "false");
             button.disabled = false;
-        })
-
-    }
-
+        });
+    };
 
     storyButtonEl.forEach((button) => {
         button.addEventListener("click", (event) => {
@@ -27,10 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 case "work":
                     projectWorkSection.setAttribute("data-status", "show");
                     projectStorySection.setAttribute("data-status", "hide");
+                    scrollSection.classList.remove("overflow-auto");
+                    // bodyEl.classList.remove("lock-scroll");
                     break;
                 case "story":
                     projectWorkSection.setAttribute("data-status", "hide");
-                    projectStorySection.setAttribute("data-status", "open");
+                    projectStorySection.setAttribute("data-status", "show");
+                    scrollSection.classList.add("overflow-auto");
+                    // bodyEl.classList.add("lock-scroll");
                     break;
                 default:
                     break;
@@ -38,6 +44,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
             clickedButton.disabled = true;
         });
-    })
+    });
+
+    // window.addEventListener('wheel', (e) => {
+    //     const atBottom = scrollSection.scrollTop + scrollSection.clientHeight >= scrollSection.scrollHeight;
+    //     const atTop = scrollSection.scrollTop === 0;
+
+    //     if ((!atBottom && e.deltaY > 0) || (!atTop && e.deltaY < 0)) {
+    //         scrollSection.scrollTop += e.deltaY;
+    //         e.preventDefault();
+    //     }
+    // }, { passive: false });
+
 
 });
