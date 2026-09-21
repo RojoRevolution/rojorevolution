9038dafd3b211c834b6037aa1083dc74c5b3518f
David Rojo <rojo@rojorevolution.com>
2026-09-21 14:35:59 -0500

    added about section to homepage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,23 +3,27 @@ const path = require('path');
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
-        res.render('index', { title: 'Human Centered Design. Off Center Differentiation.' });
+        res.render('index', { title: 'Human Centered Design. Off Center Differentiation. | RojoRevolution' });
     });
 
     app.get('/index', (req, res) => {
-        res.render('index', { title: 'Human Centered Design. Off Center Differentiation.' });
+        res.render('index', { title: 'Human Centered Design. Off Center Differentiation. | RojoRevolution' });
     });
 
     app.get('/logos', (req, res) => {
-        res.render('logos', { title: 'Logo Collection | Identity Design' });
+        res.render('logos', { title: 'Logos Logos Logos | RojoRevolution' });
     });
 
     app.get('/labels', (req, res) => {
-        res.render('labels', { title: 'Craft Beer Labels | Illustration & Packaging' });
+        res.render('labels', { title: 'Craft Beer Label Design | RojoRevolution' });
     });
 
     app.get('/acoponbrewing', (req, res) => {
-        res.render('acopon', { title: 'Acopon Brewing Co. | Differentiation, Illustration, Packaging, Print' });
+        res.render('acopon', { title: 'Acopon Brewing Co. | RojoRevolution' });
+    });
+
+    app.get('/about', (req, res) => {
+        res.render('about', { title: 'About David Rojo | RojoRevolution' });
     });
 
 }
